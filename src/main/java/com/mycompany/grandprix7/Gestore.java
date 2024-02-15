2acb5b20f28f464430967f2f6f42d05f82dbc939
DamiPaglia <damianopagliarini360@gmail.com>
2024-02-15 17:57:29 +0100

    commit pagliarini, modifica di alcuni metodi, correzzione del metodo impostazioni circuito

--- src/main/java/com/mycompany/grandprix7/Gestore.java
+++ src/main/java/com/mycompany/grandprix7/Gestore.java
@@ -15,21 +15,25 @@ import java.util.logging.Logger;
 
 
 public class Gestore {
+
     public static void main(String[] args) {
-        visualizza();
+      
+      
          Lettore l = new Lettore("user.json");
         Scanner sc = new Scanner(System.in);
 
     int s = -1;
     
     while (s != 0) {
+        visualizza();
     s = sc.nextInt();
     switch (s) {
       case 1:
-        accedi();
+      
+        Giocatore.accedi();
         break;
       case 2:
-        impostazioniCircuito();
+        Circuito.impostazioniCircuito();
         break;
       case 3:
         
@@ -46,85 +50,14 @@ public class Gestore {
         System.out.println("Seleziona una di queste scelte:******************************************************");
         System.out.println("1)Accedi con username e password*****************************************************");
         System.out.println("2)Impostazioni Circuito**************************************************************");
+        System.out.println("3)avvia gara*****************************************************************************");
         System.out.println("0)chiudi*****************************************************************************");
         int sc = input.nextInt();
         
     }
     
-    public static void impostazioniCircuito() {
-        
-       Scanner scanner1 = new Scanner(System.in);
-       
-        System.out.println("Scrivi il nome del circuito: ");
-        String nomeCircuito = scanner1.nextLine();
-        System.out.println("Scegli la lunghezza del circuito(in Km): ");
-        Double lunghezzaCircuito = scanner1.nextDouble();
-        System.out.println("Imposta il numero di giri: ");
-        int nGiri = scanner1.nextInt();
-        System.out.println("Imposta il numero di pit stop: ");
-        int nPit = scanner1.nextInt();
-        Circuito circuito= new Circuito(nomeCircuito,lunghezzaCircuito, nGiri,nPit);
-        
-        
-        
-        System.out.println("Impostazioni circuito aggiornate!");
+    
+   
     
     }
-    
-    public static void accedi() {
-    boolean accesso=false;
-    Matrice m = new Matrice("VIGENERE");
-    Vigenere v = new Vigenere(0,12,0,12,m);
-    Vigenere v1 = new Vigenere(12,26,0,12,m);
-    Vigenere v2 = new Vigenere(0,12,0,12,m);
-    Vigenere v3 = new Vigenere(12,26,12,26,m);
-    Thread t = new Thread(v);
-    Thread t1 = new Thread(v1);
-    Thread t2 = new Thread(v2);
-    Thread t3 = new Thread(v3);
-    
-    t.start();
-    t1.start();
-    t2.start();
-    t3.start();
-    try{
-    t.join();
-    t1.join();
-    t2.join();
-    t3.join();
-    }catch(InterruptedException ex){
-    System.err.println("Errore nel metodo join!");
-    }
-    
-  
-    //l.start();
-    Scanner input = new Scanner(System.in);
-    System.out.println("inserisci un usarname per accedere:");
-    String username=input.nextLine();
-    String verificaU=username.toUpperCase();
-    
-    
-    System.out.println("inserisci una password:");
-    String password=input.nextLine();
-    String verificaP = password.toUpperCase();
-    String passwordC=m.cifra(verificaP);
- 
 
-    
-    Scrittore scrittore = new Scrittore("user.json", verificaU, passwordC);
-    Thread threadScrittore = new Thread(scrittore);
-    threadScrittore.start();
-
-        try {
-            Thread.sleep(2000);
-            threadScrittore.join();
-            
-            System.out.println("\nStiamo effettuando l'accesso un secondo....\n");
-            
-        } catch (InterruptedException ex) {
-            Logger.getLogger(Gestore.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    
-    
-    }
-}
--- src/main/java/com/mycompany/grandprix7/Giocatore.java
+++ src/main/java/com/mycompany/grandprix7/Giocatore.java
@@ -5,12 +5,16 @@
 package com.mycompany.grandprix7;
 
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class Giocatore {
     private String username;
     private int password;
     private ArrayList<String> automobili;
+    private static boolean accesso=false;
         
     public Giocatore(final String username, final int password) {
         this.username = username;
@@ -18,22 +22,57 @@ public class Giocatore {
         this.automobili = new ArrayList();
         
     }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public int getPassword() {
-        return password;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public void setPassword(int password) {
-        this.password = password;
+public static void accedi() {
+    Matrice m = new Matrice("VIGENERE");
+    Vigenere v = new Vigenere(0,12,0,12,m);
+    Vigenere v1 = new Vigenere(12,26,0,12,m);
+    Vigenere v2 = new Vigenere(0,12,0,12,m);
+    Vigenere v3 = new Vigenere(12,26,12,26,m);
+    Thread t = new Thread(v);
+    Thread t1 = new Thread(v1);
+    Thread t2 = new Thread(v2);
+    Thread t3 = new Thread(v3);
+    
+    t.start();
+    t1.start();
+    t2.start();
+    t3.start();
+    try{
+    t.join();
+    t1.join();
+    t2.join();
+    t3.join();
+    }catch(InterruptedException ex){
+    System.err.println("Errore nel metodo join!");
     }
     
+  
+    //l.start();
+    Scanner input = new Scanner(System.in);
+    System.out.println("inserisci un usarname per accedere:");
+    String username=input.nextLine();
+    String verificaU=username.toUpperCase();
     
+    
+    System.out.println("inserisci una password:");
+    String password=input.nextLine();
+    String verificaP = password.toUpperCase();
+    String passwordC=m.cifra(verificaP);
+ 
+
+    
+    Scrittore scrittore = new Scrittore("user.json", verificaU, passwordC);
+    Thread threadScrittore = new Thread(scrittore);
+    threadScrittore.start();
+
+        try {
+            Thread.sleep(2000);
+            threadScrittore.join();
+            accesso=true;
+            System.out.println("\nStiamo effettuando l'accesso un secondo....\n");
+            
+        } catch (InterruptedException ex) {
+            Logger.getLogger(Gestore.class.getName()).log(Level.SEVERE, null, ex);
+        }
+}
 }
